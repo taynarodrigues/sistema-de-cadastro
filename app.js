de1644fa486b4fab8325703f1c68ce650d71442e
taynarodrigues <taynajesus@gmail.com>
2025-02-16 02:00:47 -0400

    adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 //mais um console log de teste:
 console.log("O Diogo fez o commit sem voce ver na aula!")
+console.log("Enviando para o GitHub")
