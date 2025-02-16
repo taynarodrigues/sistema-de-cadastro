56dd4f90fcb817058e2e5f63a502b347e2f6be59
dioguinhorodrigues <62912179+dioguinhorodrigues@users.noreply.github.com>
2025-02-16 01:46:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
+//mais um console log de teste:
+console.log("O Diogo fez o commit sem voce ver na aula!")
